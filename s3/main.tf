0a15a5d7387475ad6fb7dc067b2a6018c9dff318
samiksha <samikshasomnthe2000@gmail.com>
2022-08-04 15:44:58 +0530

    on m

--- s3/main.tf
+++ s3/main.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "tf_buckets" {
     count         = length(var.s3_bucket_names) //count will be 3
   bucket        = var.s3_bucket_names[count.index]
   acl           = "private"
-  region        = "ap-south-1"
+
   
 }
   
