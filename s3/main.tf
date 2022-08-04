20336193196201e6cfbef60b20237066710e7056
samiksha <samikshasomnthe2000@gmail.com>
2022-08-04 17:19:29 +0530

    all

--- s3/main.tf
+++ s3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 resource "aws_s3_bucket" "tf_buckets" {
-    count         = length(var.s3_bucket_names) //count will be 3
+    count         = length(var.s3_bucket_names) //count will be 10
   bucket        = var.s3_bucket_names[count.index]
   acl           = "private"
 
---
+++ vpc/vpc.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" vpc1{
+    cidr_block = 10.0.0.0/16
+    tags = {
+        Name = "myvpc"
+    }
+
+}
+resource "aws_subnet" "private_subnet"{
+    vpc_id = aws_vpc.vpc1.id
+    cidr_block = 10.0.0.0/20
+    availability_zone = "ap-soth-1a"
+    tags={
+        Name = "Private_subnet"
+    }
+}
+resource "aws_subnet" "public_subnet" {
+    vpc_id=aws_vpc.vpc1.id
+    cidr_block = 10.0.16.0/20
+    availability_zone ="ap-south-1b"
+    map_public_ip_on_launch = true
+    tags = {
+        Name= "Public_subnet"
+    }
+  
+}
