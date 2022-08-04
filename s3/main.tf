6de121806faf27f27e5fb4eb0265cc97c426f9d2
samiksha <samikshasomnthe2000@gmail.com>
2022-08-04 15:42:13 +0530

    all main

--- s3/main.tf
+++ s3/main.tf
@@ -1,7 +1,11 @@
 provider "aws" {
     region = "ap-south-1"
 }
-resource "aws_s3_bucket" "rugged_buckets" {
-  count         = 10
+resource "aws_s3_bucket" "tf_buckets" {
+    count         = length(var.s3_bucket_names) //count will be 3
+  bucket        = var.s3_bucket_names[count.index]
+  acl           = "private"
   region        = "ap-south-1"
+  
 }
+  
---
+++ s3/vaiable.tf
@@ -0,0 +1,16 @@
+ variable "s3_bucket_names" {
+    type = list
+    
+   default = ["bucket1.app", 
+             "bucket2.app", 
+             "bucket3.app",
+             "bucket4.app",
+             "bucket5.app",
+             "bucket6.app",
+             "bucket7.app",
+             "bucket8.app",
+             "bucket9.app",
+             "bucket10.app",
+
+            ]
+ }
